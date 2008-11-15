3e1204b020020423858665c24f793528c646fa24
attila.lendvai <attila.lendvai@gmail.com>
2008-11-15 18:04:11 +0000

    renamed to thread-safe-file-log-appender

--- src/appenders-with-threading.lisp
+++ src/appenders-with-threading.lisp
@@ -1,13 +1,13 @@
 (in-package :cl-yalog)
 
-(export '(make-threaded-file-log-appender threaded-file-log-appender) :cl-yalog)
+(export '(make-thread-safe-file-log-appender thread-safe-file-log-appender) :cl-yalog)
 
-(defclass threaded-file-log-appender (file-log-appender)
-  ((lock :initform (bordeaux-threads:make-recursive-lock "threaded-file-log-appender")
+(defclass thread-safe-file-log-appender (file-log-appender)
+  ((lock :initform (bordeaux-threads:make-recursive-lock "thread-safe-file-log-appender")
          :accessor lock-of
          :initarg :lock)))
 
-(defmethod append-message ((category log-category) (appender threaded-file-log-appender) message level)
+(defmethod append-message ((category log-category) (appender thread-safe-file-log-appender) message level)
   ;; TODO implement buffering and flushing to lower contention. needs a timer.
   (bordeaux-threads:with-recursive-lock-held ((lock-of appender))
     (with-open-file (log-file (merge-pathnames (cl-yalog::log-file-of appender) *log-directory*)
@@ -21,5 +21,5 @@
                 (cl-yalog::name-of category)
                 message)))))
 
-(defun make-threaded-file-log-appender (file-name)
-  (make-instance 'threaded-file-log-appender :log-file file-name))
+(defun make-thread-safe-file-log-appender (file-name)
+  (make-instance 'thread-safe-file-log-appender :log-file file-name))
