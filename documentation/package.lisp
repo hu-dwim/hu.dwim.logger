17d0e4c204ddd8efe476d22472c733fe159fad96
levente.meszaros <levente.meszaros@gmail.com>
2010-04-07 13:21:47 +0000

    Uniformize def package.

--- documentation/package.lisp
+++ documentation/package.lisp
@@ -4,7 +4,7 @@
 ;;;
 ;;; See LICENCE for details.
 
-(in-package :hu.dwim.util)
+(in-package :hu.dwim.def)
 
 (def package :hu.dwim.logger.documentation
   (:use :hu.dwim.asdf
@@ -15,4 +15,4 @@
         :hu.dwim.syntax-sugar
         :hu.dwim.util
         :hu.dwim.wui)
-  (:readtable-setup (enable-standard-hu.dwim-syntaxes)))
+  (:readtable-setup (hu.dwim.util:enable-standard-hu.dwim-syntaxes)))
--- source/package.lisp
+++ source/package.lisp
@@ -4,11 +4,11 @@
 ;;;
 ;;; See LICENCE for details.
 
-(in-package :hu.dwim.util)
+(in-package :hu.dwim.def)
 
 (def package :hu.dwim.logger
   (:use :hu.dwim.asdf
         :hu.dwim.common
         :hu.dwim.def
         :hu.dwim.util)
-  (:readtable-setup (enable-standard-hu.dwim-syntaxes)))
+  (:readtable-setup (hu.dwim.util:enable-standard-hu.dwim-syntaxes)))
