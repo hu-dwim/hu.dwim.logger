86d5a8c2459a15aa34c83c324131106aa3b9dd09
attila.lendvai <attila.lendvai@gmail.com>
2010-05-04 10:16:28 +0000

    setup-logging-for-production moved here and exported

--- source/standard-logger.lisp
+++ source/standard-logger.lisp
@@ -13,3 +13,12 @@
   :runtime-level (if *load-as-production?* +info+ +debug+)
   :compile-time-level (if *load-as-production?* +debug+ +dribble+)
   :appenders ((debug-only* (make-instance 'brief-stream-appender))))
+
+(def (function e) setup-logging-for-production (log-directory)
+  (assert (truename log-directory) log-directory "Log directory does not exist or is not accessible. Tried: ~S" log-directory)
+  (setf *log-directory* log-directory)
+  (bind ((standard-logger (find-logger 'standard-logger)))
+    (setf (hu.dwim.logger::appenders-of standard-logger)
+          (list (make-level-filtering-appender +warn+ (make-thread-safe-file-appender "error.log"))
+                (make-thread-safe-file-appender "root.log")))
+    (setf (log-level standard-logger) +info+)))
