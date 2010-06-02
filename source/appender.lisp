afca75dc2c88ead416ded0f9c644bd286f600420
attila.lendvai <attila.lendvai@gmail.com>
2010-06-02 14:22:09 +0000

    follow util

--- source/appender.lisp
+++ source/appender.lisp
@@ -188,13 +188,6 @@
 
 (def generic flush-caching-appender-messages (appender lines))
 
-;; TODO delme, and use something in iolib.os once it's comitted...
-;; TODO or at least move it to hu.dwim.util if that's not the way to go...
-(def (function io) get-monotonic-time ()
-  "Returns a time in seconds as a double-float that constantly grows (unaffected by setting the system clock)."
-  #-allegro (isys:get-monotonic-time)
-  #+allegro (get-internal-real-time))
-
 (def constant +caching-appender/maximum-cache-size+ 128)
 
 (def (namespace :weakness :key-or-value) caching-appender)
