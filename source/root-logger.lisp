caf11a4e4e89ec3ade21b765142a363fe85809b3
attila.lendvai <attila.lendvai@gmail.com>
2014-04-12 09:10:59 +0000

    root-logger logs to *debug-io* by default instead of *standard-output*

--- source/root-logger.lisp
+++ source/root-logger.lisp
@@ -9,7 +9,7 @@
 (def (logger e) root-logger ()
   :runtime-level (if *load-as-production?* +info+ +debug+)
   :compile-time-level (if *load-as-production?* +debug+ +dribble+)
-  :appenders ((debug-only* (make-thread-safe-stream-appender '*standard-output*)))
+  :appenders ((debug-only* (make-thread-safe-stream-appender '*debug-io*)))
   :documentation "This logger will be the direct parent of newly defined loggers, unless otherwise specified in the DEFLOGGER form. The consequence of this is that each logger will be the predecessor of this logger.")
 
 (def (function e) setup-logging-for-production (log-directory)
