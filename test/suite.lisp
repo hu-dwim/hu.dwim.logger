0f9ad8fbb7e200a0f13bc6762261eb7e3478525c
attila <attila@lendvai.name>
2015-05-31 15:24:36 +0000

    housekeeping

--- test/suite.lisp
+++ test/suite.lisp
@@ -7,3 +7,5 @@
 (in-package :hu.dwim.logger.test)
 
 (def suite* (test :in root-suite))
+
+;; well, i wouldn't call it a test suite, but i guess it's better to have at least a skeleton than not having anything...?
